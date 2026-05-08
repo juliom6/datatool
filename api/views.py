6f05a0f60b368a987ce81c4dcb4463464b32fc69
juliom6 <juliom6@gmail.com>
2026-05-08 01:57:55 -0500

    Implement cron job scheduling in CreateCluster
    Added cron job scheduling for cluster creation.

--- api/views.py
+++ api/views.py
@@ -3,17 +3,23 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView,
 from .models import Cluster
 from .serializers import ClusterSerializer
 
+from crontab import CronTab
+
 
 class CreateCluster(CreateAPIView):
     queryset = Cluster.objects.all()
     serializer_class = ClusterSerializer
 
     def perform_create(self, serializer):
-    	instance = serializer.save()
-    	self.schedule_job_cluster(instance)
-    	
+        instance = serializer.save()
+        self.schedule_job_cluster(instance)
+
     def schedule_job_cluster(self, instance):
-    	print(instance.cluster_id)
+        cron = CronTab(user=True)
+        job = cron.new(command=f'./git/datatool/run_env.sh {str(instance.cluster_id)} >> ./cron.log 2>&1')
+        job.setall(instance.trigger_at)
+        cron.write()
+        print(str(instance.cluster_id))
 
 
 class ListCluster(ListAPIView):
