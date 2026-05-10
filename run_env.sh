5ae896ffb70960fef407a0cdaa3658e08008c985
juliom6 <juliom6@gmail.com>
2026-05-09 22:45:37 -0500

    Fix paths in run_env.sh for virtual environment

--- run_env.sh
+++ run_env.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-CURRENT_USER=$(whoami)
-echo $CURRENT_USER
-source /home/julio/git/datatool/.venv/bin/activate
-python3 /home/julio/git/datatool/mp.py $1
+source /home/datatool/git/datatool/.venv/bin/activate
+python3 /home/datatool/git/datatool/mp.py $1
