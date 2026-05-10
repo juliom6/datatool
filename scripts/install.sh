50acbd1a6740f37113862b07386ff1babb85b995
juliom6 <juliom6@gmail.com>
2026-05-10 00:16:39 -0500

    Change user home directory for environment setup
    Updated user home directory from 'luser' to 'datatool' for environment variable exports and sourcing.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 spark_release="spark-3.5.3-bin-hadoop3" # "spark-4.0.1-bin-hadoop3"
 sudo apt update && sudo apt install default-jdk -y
-echo "export JAVA_HOME=$(dirname $(dirname $(update-alternatives --list javac)))" >> /home/luser/.bashrc
+echo "export JAVA_HOME=$(dirname $(dirname $(update-alternatives --list javac)))" >> /home/datatool/.bashrc
 sudo apt install megatools -y --allow-unauthenticated
 megadl "https://mega.nz/file/2NMGQDpA#48e4OMILDTPjwLqzleTVLFo_6xu-MQmfppRi4-rZlZs" --path=$spark_release.tgz # "spark-3.5.3-bin-hadoop3"
 # megadl "https://mega.nz/file/CNdATRYI#XafEnYLjCl-sPrYPe6RnXmSRegBMBzdW0-T1QIcsB9s" --path=$spark_release.tgz # "spark-4.0.1-bin-hadoop3"
 tar xvf $spark_release.tgz
 sudo mv $spark_release /opt/spark
-echo "export SPARK_HOME=/opt/spark" >> /home/luser/.bashrc
-source /home/luser/.bashrc
-echo "export PATH=$PATH:$SPARK_HOME/bin" >> /home/luser/.bashrc
-source /home/luser/.bashrc
+echo "export SPARK_HOME=/opt/spark" >> /home/datatool/.bashrc
+source /home/datatool/.bashrc
+echo "export PATH=$PATH:$SPARK_HOME/bin" >> /home/datatool/.bashrc
+source /home/datatool/.bashrc
 rm $spark_release.tgz
 wget https://repo1.maven.org/maven2/com/microsoft/azure/azure-storage/8.6.6/azure-storage-8.6.6.jar
 mv azure-storage-8.6.6.jar /opt/spark/jars/
